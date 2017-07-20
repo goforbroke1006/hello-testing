37fe69f4e1da336f4bbb0b44094e4f6b0b2288ad
Sergey <go.for.broke1006@gmail.com>
2017-07-20 21:31:56 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -5,7 +5,17 @@ php composer.phar install
 
 curl -o selenium-server-standalone.jar http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar
 
-curl -o chromedriver.zip https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip
+if [ "$(uname)" == "Darwin" ]; then
+    FILE="https://chromedriver.storage.googleapis.com/2.30/chromedriver_mac64.zip"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    FILE="https://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    FILE="https://chromedriver.storage.googleapis.com/2.30/chromedriver_win32.zip"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    FILE="https://chromedriver.storage.googleapis.com/2.30/chromedriver_win32.zip"
+fi
+
+curl -o chromedriver.zip "${FILE}"
 unzip -o chromedriver.zip && rm -f chromedriver.zip
 
 cp tests/_data/users.yml.dist tests/_data/users.yml

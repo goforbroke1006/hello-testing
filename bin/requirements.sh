4d0be151e3df7545c476f6e20951fd420c1a9f1b
Sergey <go.for.broke1006@gmail.com>
2017-11-14 14:07:14 +0300

    Update requirements.sh

--- bin/requirements.sh
+++ bin/requirements.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+WEBDRIVER_CDN="https://chromedriver.storage.googleapis.com"
 WEBDRIVER_VERSION="2.33"
+WEBDRIVER_FILE="chromedriver"
 
 curl -o composer.phar https://getcomposer.org/composer.phar
 php composer.phar install
@@ -8,13 +10,13 @@ php composer.phar install
 curl -o selenium-server-standalone.jar http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.1.jar
 
 if [ "$(uname)" == "Darwin" ]; then
-    WEBDRIVER_URL="https://chromedriver.storage.googleapis.com/${WEBDRIVER_VERSION}/chromedriver_mac64.zip"
+    WEBDRIVER_URL="${WEBDRIVER_CDN}/${WEBDRIVER_VERSION}/${WEBDRIVER_FILE}_mac64.zip"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    WEBDRIVER_URL="https://chromedriver.storage.googleapis.com/${WEBDRIVER_VERSION}/chromedriver_linux64.zip"
+    WEBDRIVER_URL="${WEBDRIVER_CDN}/${WEBDRIVER_VERSION}/${WEBDRIVER_FILE}_linux64.zip"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    WEBDRIVER_URL="https://chromedriver.storage.googleapis.com/${WEBDRIVER_VERSION}/chromedriver_win32.zip"
+    WEBDRIVER_URL="${WEBDRIVER_CDN}/${WEBDRIVER_VERSION}/${WEBDRIVER_FILE}_win32.zip"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    WEBDRIVER_URL="https://chromedriver.storage.googleapis.com/${WEBDRIVER_VERSION}/chromedriver_win32.zip"
+    WEBDRIVER_URL="${WEBDRIVER_CDN}/${WEBDRIVER_VERSION}/${WEBDRIVER_FILE}_win32.zip"
 fi
 
 curl -o chromedriver.zip "${WEBDRIVER_URL}"
